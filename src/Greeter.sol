035bbcbf23fede6961e75c57c1f0f38d6784ec1b
abcode <mabsattar@proton.me>
2024-07-30 06:29:03 +0530

    initialized greeting contract

---
+++ src/Greeter.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.19;
+
+
+import "forge-std/console.sol";
+
+contnract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        console.log("Deploying a Greeter with greeting: ", _greeting);
+        greeting = _greeting;
+    }
+
+    function greet() public view returns (string memory) {
+        return greeting;
+    }
+
+    function setGreeting(string memory _greeting) public {
+        console.log("Changingn Greeting from '%s' to '%s'", greeting, _greeting);
+        greeting = _greeting;
+    }
+
+    receive() external payable{}
+    fallback() external payable{}
+} 
